992f73fc096b424d04eb3ac65503ac239003e884
JeinJang <jeinjang99@gmail.com>
2024-11-09 17:58:11 +0900

    feat: stage 1 mini game 1

---
+++ Assets/1. Scripts/Stage1/Stage1MiniGame1ItemSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/Stage1/Mini Game 1 Item", fileName = "Stage 1 Mini Game 1 Item")]
+public class Stage1MiniGame1ItemSO : ScriptableObject
+{
+    public float posX;
+    public float posY;
+    public bool isRightItem;
+}
